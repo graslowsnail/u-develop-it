08af8702925a0b30c4d1ec1dd599a39c015454a4
graslowsnail <graslowsnail@gmail.,com>
2022-10-30 16:38:59 -0500

    connected server to db and printed rows in elections table

--- server.js
+++ server.js
@@ -3,10 +3,30 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require('mysql2');
+
+
 //express middlewear
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    socketPath : '/tmp/mysql.sock',
+    host: `localhost`,
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request ( Not Found)
 app.use((req, res) => {
